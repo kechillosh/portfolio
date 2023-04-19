965e5165dd817a3090e3fffa9cba35579f04e815
kechillosh <kechillosh@gmail.com>
2023-04-19 17:48:44 +0300

    background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import backg from "../photos/matrix-g87768af52_1920.jpg";
+import "../styles/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="backgr"></div>
+      Home
+    </div>
+  );
 }
 
 export default Home;
